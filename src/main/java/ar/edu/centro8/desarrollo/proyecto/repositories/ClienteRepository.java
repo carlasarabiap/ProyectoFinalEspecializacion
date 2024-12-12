b210c7431ce57de374ad09c0f3c739df97bf8842
Carla Sarabia <128642074+carlasarabiap@users.noreply.github.com>
2024-12-12 10:25:25 -0300

    Delete src directory

--- src/main/java/ar/edu/centro8/desarrollo/proyecto/repositories/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.centro8.desarrollo.proyecto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ar.edu.centro8.desarrollo.proyecto.models.Cliente;
-
-public interface ClienteRepository extends JpaRepository <Cliente, Long>{
-
-}
--- src/main/java/ar/edu/centro8/desarrollo/proyecto/repositories/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.centro8.desarrollo.proyecto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ar.edu.centro8.desarrollo.proyecto.models.Menu;
-
-public interface MenuRepository extends JpaRepository <Menu, Long>{
-
-}
