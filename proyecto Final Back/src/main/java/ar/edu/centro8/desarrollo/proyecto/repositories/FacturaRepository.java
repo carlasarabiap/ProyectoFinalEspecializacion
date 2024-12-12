8dbd0c97bf0eb8ff4b3e25a9bb13bb9e7f6ed4ba
Carla Sarabia <128642074+carlasarabiap@users.noreply.github.com>
2024-12-12 10:33:14 -0300

    Delete proyecto Final Back directory

--- proyecto Final Back/src/main/java/ar/edu/centro8/desarrollo/proyecto/repositories/FacturaRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.centro8.desarrollo.proyecto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ar.edu.centro8.desarrollo.proyecto.models.Factura;
-
-public interface FacturaRepository extends JpaRepository <Factura,Long>{
-
-}
